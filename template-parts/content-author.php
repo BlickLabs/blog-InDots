8b1966d5d1d1e863fe6c26e1cb8259e22b868fe0
Raúl Marty <santor3medio@gmail.com>
2018-01-10 11:03:35 -0600

    #7. Add new font class to content-author.php

--- template-parts/content-author.php
+++ template-parts/content-author.php
@@ -21,13 +21,13 @@
 	</div><!-- .author-avatar -->
 
 	<div class="author-heading">
-		<h2 class="author-title"><?php esc_html_e( 'Publicado por', 'shoreditch' ); ?></h2>
-		<h3 class="author-name"><?php echo get_the_author(); ?></h3>
+		<h2 class="author-title indots-font-LibreBaskerville-regular"><?php esc_html_e( 'Publicado por', 'shoreditch' ); ?></h2>
+		<h3 class="author-name indots-font-cabin-regular"><?php echo get_the_author(); ?></h3>
 	</div><!-- .author-heading -->
 
 	<p class="author-bio">
 		<?php the_author_meta( 'description' ); ?>
-		<a class="author-link" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
+		<a class="author-link indots-font-cabin-regular" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
 			<?php printf( esc_html__( 'Ver todas las publicaciones de %s', 'shoreditch' ), get_the_author() ); ?>
 		</a>
 	</p><!-- .author-bio -->
